e889dd5b42a7471f4706438c552f3778bb710a9d
Needles Atsuki <garlawop@gmail.com>
2020-07-11 13:09:59 +0200

    Rick : 0.5.2

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.TOKEN = "NzMwMTE4NTIxNjIyNjI2Mzk1.Xwdqcw.M83XUbid1uuSoA8fLMjIH_ODwtg";
 exports.PREFIX = "r!";
 exports.VERSION = "0.5.2";
-exports.WHITELIST = ["509115921156014081", "328615373333331970"];
+exports.WHITELIST = ["509115921156014081"];
