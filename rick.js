29bc23364ceec73c495d992351baf19283c8d223
Needles Atsuki <garlawop@gmail.com>
2020-07-12 23:58:05 +0200

    Rick : 0.6.4

--- rick.js
+++ rick.js
@@ -1,7 +1,9 @@
 const Discord                                         = require('discord.js');
 const {TOKEN, PREFIX, VERSION, WHITELIST}             = require('./config.js');
-const Neko                                            = require("neko-love");
 const client                                          = new Discord.Client({disableMentions: "everyone"});
+const { get } = require('https');
+const { measureMemory } = require('vm');
+const { parse } = require('path');
 
 client.commands = new Discord.Collection()
 
@@ -550,6 +552,52 @@ client.on('message', async message => {
   if (message.content.startsWith(prefix + "invisible")) {
     message.channel.send("️");
   }
+
+  if (message.content.startsWith(prefix + "nekotest")) {
+
+    let avatarbot = client.user.avatarURL({dynamic: true});
+
+  let erreurAPI = new Discord.MessageEmbed()
+  .setColor(couleur)
+  .setTitle("NSFW ERREUR")
+  .setDescription("Une erreur est survenue avec l'API !")
+  .setFooter('Rick🛸 ©️ Copyright : Atsuki \\/ Needles', avatarbot)
+
+  if (!message.channel.nsfw) return message.channel.send(nonsfw).catch(console.error);
+
+  get("https://neko-love.xyz/api/v1/neko", (res) => {
+      const { statusCode } = res;
+      if (statusCode !== 200) {
+          return message.channel.send(erreurAPI)
+      }
+
+      res.setEncoding("utf8");
+      let rawData = "";
+
+      res.on("data", chunk => {
+          rawData += chunk;
+      });
+
+      res.on("end", () => {
+          try {
+              const parsedData = JSON.parse(rawData);
+              
+              let image = new Discord.MessageEmbed()
+              .setColor(couleur)
+              .setTitle("NSFW")
+              .setImage(parsedData.url)
+              .setFooter('Rick🛸 ©️ Copyright : Atsuki \\/ Needles', avatarbot)
+
+              message.channel.send(image);
+          } catch (error) {
+              console.error(error.message);
+          }
+      });
+  }).on("error", (error) => {
+      console.error(error.message);
+  });
+};
+
 });
 
 client.login(token);
