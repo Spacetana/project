631de023b3de8f2ae154f9624ef3f2d7e516bc2a
Needles Atsuki <garlawop@gmail.com>
2020-07-13 01:47:21 +0200

    Rick : 0.6.5

--- rick.js
+++ rick.js
@@ -191,8 +191,8 @@ client.on('message', async message => {
     .addField("Liste des `ID` whitelist :", whitelist)
     .setFooter('Rick🛸 ©️ Copyright : Atsuki \\/ Needles', avatarbot)     
 
-    if (!user, user.id = message.author.id && !whitelist.includes(user.id)) return message.channel.send(standardliste);
-    if (!user, user.id = message.author.id && whitelist.includes(user.id)) return message.channel.send(avancéliste);
+    if (user, user.id = message.author.id && !whitelist.includes(user.id)) return message.channel.send(standardliste);
+    if (user, user.id = message.author.id && whitelist.includes(user.id)) return message.channel.send(avancéliste);
 
     if (!user && !whitelist.includes(message.author.id)) return message.channel.send(standardliste);
     if (!user && whitelist.includes(message.author.id)) return message.channel.send(avancéliste);
