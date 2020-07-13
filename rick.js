14d3f371b7315ff84e26154a0930adc056209cd2
Needles Atsuki <garlawop@gmail.com>
2020-07-13 03:40:15 +0200

    Rick : 0.6.7

--- rick.js
+++ rick.js
@@ -859,6 +859,77 @@ if (message.content.startsWith(prefix + "kiss")) {
   });
 }
 
+if (message.content.startsWith(prefix + "cry")) {
+
+  let erreurAPI = new Discord.MessageEmbed()
+  .setColor(couleur)
+  .setTitle("CRY ERREUR")
+  .setDescription("Une erreur est survenue avec l'API !")
+  .setFooter('Rick🛸 ©️ Copyright : Atsuki \\/ Needles', avatarbot)
+
+  let user = message.mentions.users.first();
+
+  get("https://neko-love.xyz/api/v1/cry", (res) => {
+
+    const { statusCode } = res;
+
+    if (statusCode !== 200) return message.channel.send(erreurAPI).catch(console.error);
+  
+    res.setEncoding("utf8");
+    let rawData = "";
+
+    res.on("data", chunk => {
+      rawData += chunk;
+    });
+
+    res.on("end", () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+
+        let user = message.mentions.users.first();
+    
+        if (user) {
+      
+          let member = message.guild.member(user);
+      
+          if (member) {    
+        
+            let image2 = new Discord.MessageEmbed()
+            .setColor(couleur)
+            .setTitle("CRY")
+            .setDescription(message.author.toString()+" fait pleurer "+user.toString()+" !")
+            .setImage(parsedData.url)
+            .setFooter('Rick🛸 ©️ Copyright : Atsuki \\/ Needles', avatarbot)
+
+            if (user && user.id - message.author.id) return message.channel.send(image2);
+          }
+        }
+
+        let image = new Discord.MessageEmbed()
+        .setColor(couleur)
+        .setTitle("CRY")
+        .setImage(parsedData.url)
+        .setFooter('Rick🛸 ©️ Copyright : Atsuki \\/ Needles', avatarbot)
+
+        let image3 = new Discord.MessageEmbed()
+        .setColor(couleur)
+        .setTitle("CRY")
+        .setDescription(message.author.toString()+" pleure tout seul !")
+        .setImage(parsedData.url)
+        .setFooter('Rick🛸 ©️ Copyright : Atsuki \\/ Needles', avatarbot)
+
+        if (!user) return message.channel.send(image);
+        if (user && user.id == message.author.id) return message.channel.send(image3);
+
+      } catch (error) {
+        console.error(error.message);
+      }
+    });
+  }).on("error", (error) => {
+    console.error(error.message);
+  });
+}
+
 });
 
 client.login(token);
