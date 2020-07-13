813bb74e8ff28df1276be1f16faff99c3de281e3
Needles Atsuki <garlawop@gmail.com>
2020-07-13 04:16:29 +0200

    Rick : 0.6.8

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.TOKEN = "NzMwMTE4NTIxNjIyNjI2Mzk1.Xwdqcw.M83XUbid1uuSoA8fLMjIH_ODwtg";
 exports.PREFIX = "r!";
-exports.VERSION = "0.6.7";
+exports.VERSION = "0.6.8";
 exports.WHITELIST = ["509115921156014081", "553543583710445568"];
