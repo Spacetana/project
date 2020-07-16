1242fa2a7c3914b2a08eef7aa25ffac084c33dc8
Needles Atsuki <garlawop@gmail.com>
2020-07-16 02:27:30 +0200

    Rick : 0.7.1

--- rick.js
+++ rick.js
@@ -104,9 +104,9 @@ client.on('message', async message => {
     .setColor(couleur)
     .setTitle("Page 4/"+totalpage+" - Commandes FUN :")
     .setDescription("**Pour tout problème avec le bot, voici le support :** **[CLIQUE ICI](https://discord.gg/4fZhCWr)**")
-    .addField(`\`${PREFIX}8ball [question]\``, "Vous permet de poser une question lambda au bot")
-    .addField(`\`${PREFIX}invisible\``, "Vous permet de faire envoyer un msg totalement invisible par le bot")
-    .addField(`\`${PREFIX}genmdp (N) (S)\``, "Permet de générer un MDP(N = ajout de nombre | S = ajout de symbole (dans le mdp))")
+    .addField(`\`${PREFIX}8ball [question]\``, "Permet de poser une question lambda au bot")
+    .addField(`\`${PREFIX}invisible\``, "Permet de faire envoyer un msg totalement invisible par le bot")
+    .addField(`\`${PREFIX}genmdp (N) (S)\``, "Permet de générer un MDP**(N = ajout de nombre | S = ajout de symbole)**")
     .setFooter('Rick🛸 ©️ Copyright : Atsuki \\/ Needles', avatarbot)
     msg.channel.send(mod);
   }
