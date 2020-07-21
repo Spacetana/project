302406e91aefe1ec24838dea8c3fd9ae78859a69
Needles Atsuki <garlawop@gmail.com>
2020-07-21 05:48:15 +0200

    Rick : 0.7.2

--- rick.js
+++ rick.js
@@ -1555,7 +1555,6 @@ if (msg.content.startsWith(prefix + 'tz') || msg.content.startsWith(prefix + 'ti
   .addField("Continent :", ":flag_us: Amérique du Nord")
   .setFooter(Copyright, avatarbot)
 
-
   if (msg.content.includes("portorico") + msg.content.includes("Puerto_Rico") + msg.content.includes("Puerto_rico") + msg.content.includes("pr") + msg.content.includes("puertorico") + msg.content.includes("Puertorico") + msg.content.includes("puerto_rico")) return msg.channel.send(Puerto_Rico).catch(console.error);
   if (msg.content.includes("Denver") + msg.content.includes("denver")) return msg.channel.send(Denver).catch(console.error);
 
