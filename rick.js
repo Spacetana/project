008157ab3f076cdf64463ab929a2f31b20904ccd
Needles Atsuki <garlawop@gmail.com>
2020-07-23 04:11:23 +0200

    Rick : 0.7.4

--- rick.js
+++ rick.js
@@ -515,7 +515,7 @@ client.on('message',  async message => {
   let pornsearchEm = new Discord.MessageEmbed()
   .setFooter(Copyright, avatarbot)
 
-  let page = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  let page = [1, 2, 3, 4, 5];
 
   if (msg.content.startsWith(prefix + "gifsearch") || msg.content.startsWith(prefix + "gs")) {
     if (!msg.channel.nsfw) return msg.channel.send(nonNsfw).catch(console.error);
