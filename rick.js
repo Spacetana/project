890472bf574391101da1e267300c73d2b725a27b
Needles Atsuki <garlawop@gmail.com>
2020-07-23 05:26:50 +0200

    Rick : 0.7.4

--- rick.js
+++ rick.js
@@ -493,8 +493,10 @@ client.on('message',  async message => {
               .setDescription("❌ Veuillez poser une question !")
               .setFooter(Copyright, avatarbot)
 
-    if (!question && message.content.includes("?")) return msg.channel.send(NoQuestion);
-    if (msg.content.includes("ça va") && msg.content.includes("cv")) return msg.channel.send(çava[Math.floor(Math.random() * (çava.length))]);
+    if (!question) return msg.channel.send(NoQuestion);
+    if (!msg.content.includes("?")) return msg.channel.send(NoQuestion);
+    if (msg.content.includes("ça va")) return msg.channel.send(çava[Math.floor(Math.random() * (çava.length))]);
+    if (msg.content.includes("cv")) return msg.channel.send(çava[Math.floor(Math.random() * (çava.length))]);
     //if (message.content.includes(tfqcase.toLowerCase())) return msg.channel.send(tfq[Math.floor(Math.random() * (tfq.length))]);
     if (question) return msg.channel.send(replies[Math.floor(Math.random() * (replies.length))])    
   }
